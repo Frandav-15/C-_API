b1b1ae98ec5de81a2491d7ec237d623c6777d1d3
Frandav-15 <davilamaldonadofrancisco@gmail.com>
2025-08-03 10:31:17 -0500

    Migraions

---
+++ MyFirst_Api/Data/MyAppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirst_Api.Models;
+
+namespace MyFirst_Api.Data
+{
+    public class MyAppContext : DbContext
+    {
+
+        
+        
+    public MyAppContext(DbContextOptions<MyAppContext> options): base(options){}
+        
+
+
+        public DbSet<Employed> employeds { get; set; }
+
+    }
+}
---
+++ MyFirst_Api/Models/Employed.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirst_Api.Models
+{
+    public class Employed
+
+    {
+
+        public int Id { get; set; }
+        public  required string Name { get; set; }
+        public string? Phone { get; set; }
+        public required string Email { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
